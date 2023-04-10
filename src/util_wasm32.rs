6e8d44d6d9c6bca2edb1e3fdc3664a90ae4977bf
Yann Hamon <yann.hamon@contentful.com>
2023-04-10 17:10:51 +0200

    iteration++

--- src/util_wasm32.rs
+++ src/util_wasm32.rs
@@ -45,20 +45,34 @@ pub async fn ttfb_js(input: String, allow_insecure_certificates: bool) -> Result
         Err(e) => return Err(JsValue::from(&*format!("error retrieving"))),
     };
 }
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Resource {
     pub name: String,
     pub domainLookupStart: u64,
     pub domainLookupEnd: u64,
 }
 
+pub fn foo () {
+    log("BAAAAAHHHH\n")
+}
+
+
+#[wasm_bindgen]
+pub fn sleep(ms: i32) -> js_sys::Promise {
+    js_sys::Promise::new(&mut |resolve, _| {
+        web_sys::window()
+            .unwrap()
+            .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, ms)
+            .unwrap();
+    })
+}
 pub async fn ttfb(input: String, _allow_insecure_certificates: bool) -> Result<TtfbOutcome, TtfbError> {
     let window = web_sys::window().expect("should have a window in this context");
     let mut opts = RequestInit::new();
     opts.method("GET");
     opts.mode(RequestMode::Cors);
     log("foo");
-    let url = format!("https://api.github.com/repos/{}/branches/master", "foo");
+    let url = format!("https://yann.mandragor.org/");
 
     let request:Request = Request::new_with_str_and_init(&url, &opts).unwrap();
 
@@ -69,23 +83,34 @@ pub async fn ttfb(input: String, _allow_insecure_certificates: bool) -> Result<T
     assert!(resp_value.is_instance_of::<Response>());
     let resp: Response = resp_value.dyn_into().unwrap();
 
+    let window = web_sys::window().unwrap();
     let performance = window
         .performance()
         .expect("performance should be available");
 
+    JsFuture::from(sleep(1500)).await;
+
     let resources = performance.get_entries_by_type("resource");
     let mut a: String = "".to_string();
+    let mut res: Resource = Resource{
+        name: "".to_string(),
+        domainLookupStart: 0,
+        domainLookupEnd: 0,
+    };
     for item in resources.iter() {
         let b: Resource = serde_wasm_bindgen::from_value(item).unwrap();
-        let c = serde_json::to_string(&b).unwrap();
-        a.push_str(c.as_str());
+        log (b.name.as_str());
+        if b.name == url {
+            res = b.clone();
+            break
+        }
     }
     // Convert this other `Promise` into a rust `Future`.
-    let json = resp.json();
+    let c = serde_json::to_string(&res).unwrap();
 
     Ok(TtfbOutcome::new(
         "foo".to_string(),
-        a,
+        c,
         66,
         100,
         10,
